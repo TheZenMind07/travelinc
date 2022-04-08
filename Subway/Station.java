b144269874ac27e9be230c7bd4dec2642401b4d1
TheModernZen07 <rajkp7598.com>
2022-04-08 23:08:49 +0530

    Station Class

--- Subway/Station.java
+++ Subway/Station.java
@@ -1,5 +1,28 @@
 package travelinc.Subway;
 
 public class Station {
-    
+    private String name;
+
+    public Station(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean equals(Object obj){
+        if(obj instanceof Station){
+            Station st = (Station)obj;
+            if(st.getName().equalsIgnoreCase(name)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int hashCode(){
+        return name.toLowerCase().hashCode();
+    }
+
 }
